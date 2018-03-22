78d04d14acfb02f3d6742bc33445cba40dae3690
Dragutin Sreckovic <35846309+SDrag@users.noreply.github.com>
2018-03-22 13:53:32 +0000

    Update exercise6.py
    comment updates

--- exercise6.py
+++ exercise6.py
@@ -1,16 +1,15 @@
 # Dragutin Sreckovic, 2018-03-06
 
-# Function for factorial calculation of a given number:
-
 def factorial(n):
+	"""Calculates factorial of a given integer and returns it."""
 	fact = 1
-	for i in range(n, 1, -1):
+	for i in range(n, 1, -1):    # Starting from the given number and going backwards
 		fact = fact * i
 	return fact
 
 
-print(factorial(5))		# prints 120
-print(factorial(7))		# prints 5040
+print(factorial(5))	# prints 120
+print(factorial(7))	# prints 5040
 print(factorial(10))	# prints 3628800
 
 # For the completeness, we should also test if 'n' is a positive integer
