bf3db60bef427acb9686631867b82ed53f1edca7
Dragutin Sreckovic <35846309+SDrag@users.noreply.github.com>
2018-03-22 13:44:17 +0000

    Update exercise4.py
    mostly comment updates

--- exercise4.py
+++ exercise4.py
@@ -5,19 +5,23 @@
 # of the numbers from 1 to 20 without any remainder
 
 # After running it got the following output:
-#The smallest number that can be divided by each of the numbers from 1 to 20 without any remainder is: 232792560
+# The smallest number that can be divided by each of the numbers from 1 to 20 without any remainder is: 232792560
 
-notYetFound = True
-finalNubmer = 20
+notYetFound = True   # Variable which gets False when we find our number -> exit while loop
+finalNubmer = 20     # Variable that holds the number we are checking
 
+# Loop until the number is found -> notYetFound becomes False:
 while notYetFound:
-    finalNubmer = finalNubmer + 10 #10 because it has to be divideable by both 2 and 10
-    divideableByAll = True
+    finalNubmer = finalNubmer + 20   # Increment by 20 because it has to be divideable by both 2 and 10
+    divideableByAll = True           # Assume True and try to dissaprove it
+    # Check if dividible by all the numbers in the range except 1:
     for i in range(2, 21):
         if finalNubmer % i > 0:
-            divideableByAll = False
-            break
+            divideableByAll = False  # Not dividible by this number, so set it to False
+            break                    # Break the for loop since we found at least one number which does'n satisfy criteria
+    # Check if the number is divideable by all:
     if divideableByAll:
-        notYetFound = False
+        notYetFound = False          # If we found our number, set this to False so we can exit while loop
 
+# Print the result:
 print("The smallest number that can be divided by each of the numbers from 1 to 20 without any remainder is:",finalNubmer)
